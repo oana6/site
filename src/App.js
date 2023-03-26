8b21b88553a25490ac61c4097039da2c03cf3a1e
Oana Balaceanu <oana.balaceanu@outlook.com>
2023-03-26 23:12:29 +0200

    basic navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/NavBar';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from 'react-bootstrap'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Contact } from './pages/Contact';
+import { Home } from './pages/Home';
+import { Blog } from './pages/Blog';
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Navbar bg="dark" variant='dark' expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">LOGO</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+
+      <div>
+        <Routes>
+
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+
+      </div>
+
+    </BrowserRouter>
   );
 }
 
